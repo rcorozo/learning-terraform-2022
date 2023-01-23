ec63c00acdae9a0fb03c37453ef499b8eb3ac394
Rodrigo Corozo <102171121+rcorozo@users.noreply.github.com>
2023-01-23 11:16:58 -0500

    Getting subnet_id

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  subnet_id                   = module.blog_vpc.public_subnets[0].id
+  subnet_id                   = "${element(module.blog_vpc.public_subnets, 0)}"
   associate_public_ip_address = true
   vpc_security_group_ids      = [module.blog_sg.security_group_id]
 
