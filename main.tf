1b3ed68948f81abd00f7da7e3309dd162af83426
Rodrigo Corozo <102171121+rcorozo@users.noreply.github.com>
2023-01-23 11:43:04 -0500

    autoscaling

--- main.tf
+++ main.tf
@@ -31,29 +31,23 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+  
+  name     = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
-  subnet_id                   = "${element(module.blog_vpc.public_subnets, 0)}"
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [module.blog_sg.security_group_id]
-
-/*   root_block_device {
-    delete_on_termination = true
-    volume_size           = 10
-    volume_type           = "gp2"
-    tags                  = {
-      Name = "Terraform Root Block Device"
-    }
-  }
- */
-  tags = {
-    Name = "HelloWorld"
-  }
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
