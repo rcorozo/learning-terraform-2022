ab5af65526a64effb566f098f1881099140d1bbb
Rodrigo Corozo <102171121+rcorozo@users.noreply.github.com>
2023-01-23 11:10:25 -0500

    Testing some VPC code

--- main.tf
+++ main.tf
@@ -35,7 +35,9 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id                   = module.blog_vpc.public_subnets[0].id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [module.blog_sg.security_group_id]
 
 /*   root_block_device {
     delete_on_termination = true
